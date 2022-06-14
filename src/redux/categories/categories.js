90ec408aa489041c2b441a75c3617ec8b2114eee
Talha <tkz9696@gmail.com>
2022-06-14 22:56:09 +0500

    Remove unnecessary comments

--- src/redux/categories/categories.js
+++ src/redux/categories/categories.js
@@ -1,10 +1,3 @@
-// define an action type for checking the status
-// set the initial state to be an empty array of categories
-// export action creators for your actions
-// write your reducer and export it as default
-//      for the check status action return a string "under construction"
-//      in case of unknown action - return the current state
-
 const categories = [];
 
 export default function reducer(state = categories, action) {
