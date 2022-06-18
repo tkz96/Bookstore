33cf11faa31dd4c03e6ca0fac74c195d01544e96
Talha <tkz9696@gmail.com>
2022-06-19 00:17:29 +0500

    Added user icon

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,18 +1,23 @@
 import { Link } from 'react-router-dom';
 import '../styles/Header.css';
+import userIcon from '../styles/user-icon.png';
 
 export default function Header() {
   return (
     <header>
       <h1>Bookstore CMS</h1>
-      <ul>
-        <li>
-          <Link to="/">BOOKS</Link>
-        </li>
-        <li>
-          <Link to="/Categories">CATEGORIES</Link>
-        </li>
-      </ul>
+      <div className="header-alignment">
+        <ul>
+          <li>
+            <Link to="/">BOOKS</Link>
+          </li>
+          <li>
+            <Link to="/Categories">CATEGORIES</Link>
+          </li>
+        </ul>
+        <img className="user-icon" src={userIcon} alt="user icon" />
+      </div>
+
     </header>
   );
 }
